3f487782d6f2ee1d2bc3d120436bc027e002256b
TemitopeAde22 <temitope224468@gmail.com>
2025-06-30 22:40:50 +0100

    first commit

--- output.tf
+++ output.tf
@@ -1,10 +1,2 @@
-locals {
-  environment = var.environment
-  region      = var.resource_group_location
-  resource_name_prefix = "${var.resource_group_location}-${var.environment}"
-  common_tags = {
-    environment = var.environment
-    region      = var.resource_group_location
-  }
-}
+
 
