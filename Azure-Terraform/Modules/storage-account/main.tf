b3ee49cd19e5b7c3ae1d16c7c8a69eb1faaa7d08
TemitopeAde22 <temitope224468@gmail.com>
2025-07-01 22:38:41 +0100

    added kubernetes node pool

--- Azure-Terraform/Modules/storage-account/main.tf
+++ Azure-Terraform/Modules/storage-account/main.tf
@@ -4,14 +4,14 @@ data "azurerm_resource_group" "myrg" {
 
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = "ukwestdevstorageaccount"
-  resource_group_name      = data.azurerm_resource_group.tfstate.name
-  location                 = data.azurerm_resource_group.tfstate.location
+  resource_group_name      = data.azurerm_resource_group.myrg.name
+  location                 = data.azurerm_resource_group.myrg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "my_storage_account_container" {
   name                  = "ukwestdev-container"
-  storage_account_id    = azurerm_storage_account.tfstate.id
+  storage_account_id    = azurerm_storage_account.myrg.id
   container_access_type = "private"
 }
