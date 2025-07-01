c61e983048da9bbbf9c75f9dd2a38253897adead
TemitopeAde22 <temitope224468@gmail.com>
2025-07-01 21:56:22 +0100

    added kubernetes node pool

--- Azure-Terraform/Modules/storage-account/main.tf
+++ Azure-Terraform/Modules/storage-account/main.tf
@@ -1,17 +1,17 @@
-# data "azurerm_resource_group" "myrg" {
-#   name = "ukwest-dev-demo-resource-group"
-# }
+data "azurerm_resource_group" "myrg" {
+  name = "ukwest-dev-demo-resource-group"
+}
 
-# resource "azurerm_storage_account" "my_storage_account" {
-#   name                     = "ukwestdevstorageaccount"
-#   resource_group_name      = data.azurerm_resource_group.tfstate.name
-#   location                 = data.azurerm_resource_group.tfstate.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "ukwestdevstorageaccount"
+  resource_group_name      = data.azurerm_resource_group.tfstate.name
+  location                 = data.azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# resource "azurerm_storage_container" "my_storage_account_container" {
-#   name                  = "ukwestdev-container"
-#   storage_account_id    = azurerm_storage_account.tfstate.id
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "my_storage_account_container" {
+  name                  = "ukwestdev-container"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
--- Azure-Terraform/backend.tf
+++ Azure-Terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ukwest-dev-demo-resource-group"
-    storage_account_name = "ukwestdevstorageaccount"   
-    container_name       = "ukwestdev-container"        
-    key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "ukwest-dev-demo-resource-group"
+#     storage_account_name = "ukwestdevstorageaccount"   
+#     container_name       = "ukwestdev-container"        
+#     key                  = "terraform.tfstate"
+#   }
+# }
