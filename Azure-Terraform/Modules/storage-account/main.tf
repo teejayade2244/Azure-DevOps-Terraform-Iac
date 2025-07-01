eff2b4aa281e836819d98c05ec3f1e4d623c0537
TemitopeAde22 <temitope224468@gmail.com>
2025-07-01 22:55:46 +0100

    added kubernetes node pool

--- Azure-Terraform/Modules/storage-account/main.tf
+++ Azure-Terraform/Modules/storage-account/main.tf
@@ -1,11 +1,9 @@
-data "azurerm_resource_group" "myrg" {
-  name = var.resource_group_name
-}
 
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.myrg.name
-  location                 = data.azurerm_resource_group.myrg.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  tags                     = var.common_tags
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 }
--- Azure-Terraform/Modules/storage-account/variable.tf
+++ Azure-Terraform/Modules/storage-account/variable.tf
@@ -3,6 +3,16 @@ variable "resource_group_name" {
   type        = string
 }
 
+variable "resource_group_location" {
+  description = "The Azure region where the resource group will be created."
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources."
+  type        = map(string)
+}
+
 variable "storage_account_name" {
   description = "The name of the storage account."
   type        = string
