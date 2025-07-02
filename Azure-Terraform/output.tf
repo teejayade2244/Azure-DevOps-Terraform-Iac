1367be51afab5b9e12d8d123e25e57456717ef51
TemitopeAde22 <temitope224468@gmail.com>
2025-07-02 22:33:26 +0100

    added Network contributor role

--- Azure-Terraform/output.tf
+++ Azure-Terraform/output.tf
@@ -1,5 +1,5 @@
 
- output "argocd_app_gateway_private_ip" {
-    value       = azurerm_application_gateway.web_app_gateway.frontend_ip_configuration[0].private_ip_address
-    description = "The private IP address of the Application Gateway for Argo CD UI access."
-  }
+#  output "argocd_app_gateway_private_ip" {
+#     value       = azurerm_application_gateway.web_app_gateway.frontend_ip_configuration[0].private_ip_address
+#     description = "The private IP address of the Application Gateway for Argo CD UI access."
+#   }
